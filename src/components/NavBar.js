91ad5da446091006e0205cea282495a303511e99
Dani Crosby <dani.crosby1111@gmail.com>
2021-06-02 23:20:50 -0500

    titled nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,7 +30,7 @@ const NavBar = ({ user }) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <Link className="navbar-brand" to="/">NavBar</Link>
+        <Link className="navbar-brand" to="/">Lucid</Link>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
