abf09565065a4c5d5f47850e5c76493757578fb4
Dani Crosby <dani.crosby1111@gmail.com>
2021-06-03 00:44:54 -0500

    added date field

--- src/components/DreamCard.js
+++ src/components/DreamCard.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import {
   Button, Card, CardTitle, Container, Row,
   Col, Collapse, CardBody, CardText
@@ -7,6 +9,18 @@ import {
 import PropTypes from 'prop-types';
 import { deleteDream } from '../helpers/data/DreamData';
 import DreamForm from './DreamForm';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
 
 const DreamCard = ({
   firebaseKey,
@@ -17,6 +31,7 @@ const DreamCard = ({
   const [editing, setEditing] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const history = useHistory();
+  const classes = useStyles();
 
   const toggle = () => setIsOpen(!isOpen);
 
@@ -43,15 +58,28 @@ const DreamCard = ({
         <Col sm="12" md={{ size: 6, offset: 3 }}>
           <Card className="card-grey">
             <Card body className="card-white">
-              <Row><div className="top-text">
+              <Row>
+                <div className="top-text">
                 <CardTitle tag="h5">{name}</CardTitle>
                 <div><i className="material-icons dream-type-icon"> cloud </i></div>
-              </div>
+                </div>
               </Row>
               <Row><div>_______________________________________________</div></Row>
-              <Row><div className="date">november 11, 2011</div></Row>
+              <Row>
+                <div className="date">
+                  <form className={classes.container} noValidate>
+                    <TextField
+                      id="date"
+                      type="date"
+                      defaultValue="2017-05-24"
+                      className={classes.textField}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </form></div></Row>
               <div>
-                <Button color="transparent" style={{ marginBottom: '1rem' }} onClick={() => handleClick('edit')}>
+                <Button color="transparent float-right" style={{ marginBottom: '1rem' }} onClick={() => handleClick('edit')}>
                   {editing ? 'Done' : 'Edit'}
                 </Button>
                 {
@@ -63,7 +91,6 @@ const DreamCard = ({
                     entry={entry}
                   />
                 }
-                           </div>
                 <Button color="transparent" onClick={toggle} style={{ marginBottom: '1rem' }}>
                   <i className="material-icons" id="expand-arrow"> keyboard_arrow_down </i></Button>
                 <Collapse isOpen={isOpen}>
@@ -77,6 +104,7 @@ const DreamCard = ({
                     </CardBody>
                   </Card>
                 </Collapse>
+              </div>
             </Card>
           </Card>
         </Col>
