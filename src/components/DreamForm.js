9361cabf517bb73e4c68f56210145adf44fa29ab
Dani Crosby <dani.crosby1111@gmail.com>
2021-06-03 21:34:27 -0500

    added textarea input form to add form

--- src/components/DreamForm.js
+++ src/components/DreamForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import {
-  Button, Form, FormGroup, Label,
+  Button, Form, FormGroup,
   Input, Card, CardHeader, Container,
-  CardFooter, CardBody, CardTitle,
+  CardFooter, CardBody,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { addDream, updateDream } from '../helpers/data/DreamData';
@@ -52,34 +52,33 @@ const DreamForm = ({
       <Card className="add-dream-card">
         <CardHeader><h2>{formTitle}</h2></CardHeader>
         <CardBody className="add-dream-card-body">
-          <CardTitle tag="h5">Special Title Treatment</CardTitle>
+
           <Form id='add-dream-form' autoComplete='off' onSubmit={handleSubmit}>
-          <FormGroup>
-            <Label for="name"></Label>
-            <Input
-              name='name'
-              id='name'
-              value={dream.name}
-              type='text'
-              placeholder='Enter Dream Title'
-              onChange={handleInputChange}
-            />
-          </FormGroup>
 
           <FormGroup>
-            <Label for="entry"></Label>
-            <Input
-              name='entry'
-              id='entry'
-              value={dream.entry}
-              type='text'
-              placeholder='Dream Description'
-              onChange={handleInputChange}
-            />
-          </FormGroup>
+              <Input
+                name='name'
+                id='name'
+                value={dream.name}
+                type='text'
+                placeholder='Enter Dream Title'
+                onChange={handleInputChange}
+              />
+            </FormGroup>
 
-          <Button className="add-btn-submit float-right" type='submit'>Submit</Button>
-        </Form>
+            <FormGroup>
+              <Input
+                name='entry'
+                id='entry'
+                value={dream.entry}
+                type='textarea'
+                placeholder='Dream Description'
+                onChange={handleInputChange}
+              />
+            </FormGroup>
+
+            <Button className="add-btn-submit float-right" type='submit'>Submit</Button>
+          </Form>
         </CardBody>
         <CardFooter>Footer</CardFooter>
       </Card>
--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { NavItem } from 'reactstrap';
 
 export default function Welcome() {
   return (
     <div className="welcome-page">
       <div className="welcome-page-content">
           <div className="welcome-title"><h1>welcome</h1></div>
-          <div className="cloud-container"><a href="./HomePage.js"><i className="material-icons" id="cloud"> wb_cloud </i></a></div>
+
+          <NavItem className="cloud-container">
+        <Link className="nav-link" to="/dreams"><i className="material-icons" id="cloud"> wb_cloud </i></Link>
+      </NavItem>
       </div>
     </div>
   );
