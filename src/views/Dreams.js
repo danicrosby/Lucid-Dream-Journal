2cb78539af0333fddf6cc0abc4e2d0e940c913d5
Dani Crosby <dani.crosby1111@gmail.com>
2021-06-18 22:29:46 -0500

    dynamic tags rendering and functional

--- src/views/Dreams.js
+++ src/views/Dreams.js
@@ -19,9 +19,9 @@ function Dreams({ user }) {
   return (
     <>
       <Container className="recorded-dream-container">
-        <NavBar user={user} />
+      <NavBar user={user} />
         <center>
-          <Link className="nav-link add-dream-btn-link mr-5 mt-4" to="/add-dream">
+          <Link className="nav-link add-dream-btn-link mr-5 mt-3" to="/add-dream">
             <Fab className="fab-add-btn add-icon-btn" color="dark"><AddIcon style={{ color: 'purple' }}/></Fab>
           </Link>
         </center>
