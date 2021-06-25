89e6227a37d42a36c00c15accd1fa4957071c6f4
Dani Crosby <dani.crosby1111@gmail.com>
2021-06-25 16:46:38 -0500

    fixed stats page cards

--- src/views/Stats.js
+++ src/views/Stats.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   Card,
   CardBody,
-  CardImg
+  CardImg,
+  CardText
 } from 'reactstrap';
 import DreamTypeStats from '../styles/Assets/DreamTypeStats.png';
 import Recurring from '../styles/Assets/RecurringStats.png';
@@ -10,22 +11,25 @@ import ColorStats from '../styles/Assets/ColorStats.png';
 
 function Education() {
   return (
-    <div className="stats-sub-container">
+    <div className="education-sub-container">
       <div className="education-card-container">
-        <Card className="education-card">
-          <CardBody className="education-card-body">
+        <Card className="stats-card">
+          <CardText>Dream Type Stats</CardText>
+          <CardBody className="stats-card-body">
           <CardImg top width="100%" src={DreamTypeStats} />
           </CardBody>
         </Card>
 
-        <Card className="education-card">
-          <CardBody className="education-card-body">
+        <Card className="stats-card">
+        <CardText>Recurring Dream Stats</CardText>
+          <CardBody className="stats-card-body">
           <CardImg top width="100%" src={Recurring} />
           </CardBody>
         </Card>
 
-        <Card className="education-card">
-          <CardBody className="education-card-body">
+        <Card className="stats-card">
+        <CardText>Color Stats</CardText>
+          <CardBody className="stats-card-body">
           <CardImg top width="100%" src={ColorStats} />
           </CardBody>
         </Card>
