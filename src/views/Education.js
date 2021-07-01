7131391ee9055cf1b8ddde898fb857fe90c27cde
Dani Crosby <dani.crosby1111@gmail.com>
2021-07-01 18:23:22 -0500

    added education checkboxes

--- src/views/Education.js
+++ src/views/Education.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {
   Card,
   CardBody,
+  FormGroup,
+  Label,
+  Input
 } from 'reactstrap';
 import RealityMenu from '../components/RealityMenu';
 import CalmMenu from '../components/CalmMenu';
@@ -17,6 +20,14 @@ function Education() {
               <h6 className="sub-card-title">Reality Checks</h6>
               <RealityMenu />
             </div>
+            <form>
+              <FormGroup className="check-form" check>
+                <Label check><Input type="checkbox" />{' '}Am I dreaming?</Label>
+                <Label check><Input type="checkbox" />{' '}Is time standing still?</Label>
+                <Label check><Input type="checkbox" />{' '}Is there anything odd about your environment?</Label>
+                <Label check><Input type="checkbox" />{' '}Are you in a state of confusion?</Label>
+              </FormGroup>
+            </form>
           </CardBody>
         </Card>
 
@@ -26,6 +37,14 @@ function Education() {
               <h6>Lucid Dream Techniques</h6>
               <LucidMenu />
             </div>
+            <form>
+              <FormGroup className="check-form" check>
+                <Label check><Input type="checkbox" />{' '}Look at hands and feet</Label>
+                <Label check><Input type="checkbox" />{' '}Push finger through palm</Label>
+                <Label check><Input type="checkbox" />{' '}Is there anything odd about your environment?</Label>
+                <Label check><Input type="checkbox" />{' '}Wake back to bed alarm?</Label>
+              </FormGroup>
+            </form>
           </CardBody>
         </Card>
 
@@ -35,6 +54,14 @@ function Education() {
               <h6>Calming Sounds</h6>
               <CalmMenu />
             </div>
+            <form>
+              <FormGroup className="check-form" check>
+                <Label check><Input type="checkbox" />{' '}Stress Relief Count Down</Label>
+                <Label check><Input type="checkbox" />{' '}Daily Meditation</Label>
+                <Label check><Input type="checkbox" />{' '}The Acid (Tumbling Lights)</Label>
+                <Label check><Input type="checkbox" />{' '}White Noise</Label>
+              </FormGroup>
+            </form>
           </CardBody>
         </Card>
       </div>
